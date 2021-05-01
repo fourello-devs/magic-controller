eef531345b55a399bbe096be8d30a36897d962c1
James Carlo Luchavez <jsluchavez@up.edu.ph>
2021-05-02 06:21:39 +0800

    Fixing customResponse

--- helpers/CustomHelpers.php
+++ helpers/CustomHelpers.php
@@ -4,9 +4,12 @@
  * @author Jeremy Layson <jeremy.b.layson@gmail.com>
  * @since 2020/11/24
  */
+
+use FourelloDevs\MagicController\ExtendedResponse;
+
 if (!function_exists('customResponse')) {
   function customResponse($data = NULL, $message = NULL)
   {
-    return new \App\Helpers\ExtendedResponse($data, $message);
+    return new ExtendedResponse($data, $message);
   }
 }
