31969ad23d0ddaa4cad75841622fa86f776242b5
James Carlo Luchavez <jsluchavez@up.edu.ph>
2021-05-02 20:46:28 +0800

    Testing fixes for customResponse (9)

--- helpers/CustomHelpers.php
+++ helpers/CustomHelpers.php
@@ -5,14 +5,16 @@
  * @since 2020/11/24
  */
 
+use FourelloDevs\MagicController\ExtendedResponse;
+
 if (! function_exists('customResponse')) {
     /**
      * @param array|null $data
      * @param string|null $message
      * @return FourelloDevs\MagicController\ExtendedResponse
      */
-    function customResponse(?array $data = NULL, ?string $message = NULL): \FourelloDevs\MagicController\ExtendedResponse
+    function customResponse(?array $data = NULL, ?string $message = NULL): ExtendedResponse
     {
-        return new FourelloDevs\MagicController\ExtendedResponse($data, $message);
+        return new ExtendedResponse($data, $message);
     }
 }
