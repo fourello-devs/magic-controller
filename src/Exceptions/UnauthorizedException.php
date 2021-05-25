5e59782d6a0c5ce10ef288ee2675f2d47cc31fa0
James Carlo Luchavez <jsluchavez@up.edu.ph>
2021-05-26 06:01:05 +0800

    Added custom message on UnauthorzedException

--- src/Exceptions/UnauthorizedException.php
+++ src/Exceptions/UnauthorizedException.php
@@ -4,14 +4,20 @@ namespace FourelloDevs\MagicController\Exceptions;
 
 use Exception;
 use Illuminate\Http\JsonResponse;
+use Throwable;
 
 class UnauthorizedException extends Exception
 {
+    public function __construct($message = "")
+    {
+        parent::__construct($message, null);
+    }
+
     public function render($request): JsonResponse
     {
         return customResponse()
             ->data([])
-            ->message('You do not have the necessary permission to access this resource.')
+            ->message($this->getMessage() ?: 'You do not have the necessary permission to access this resource.')
             ->failed(403)
             ->generate();
     }
